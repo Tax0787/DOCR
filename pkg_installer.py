0a399fce66e6f1f50e8590dd373a7796aa6fb205
Tax0787 <93974004+Tax0787@users.noreply.github.com>
2023-09-15 07:43:07 +0000

    fix...3.....

--- pkg_installer.py
+++ pkg_installer.py
@@ -1,61 +1,67 @@
-from os import system as s
+from os import system as s2
 from os import mkdir
 from os import chdir as cd
 from sys import argv as a
 
 
-def just_install(pkg, ver=''):
-  s(f'python -m pip{ver} install {pkg}')
+def s(x, prints=False):
+  if prints:
+    print(f'\t#{x}')
+  s(x)
 
 
-def updatepip(ver=''):
-  just_install(f'--upgrade pip{ver}', ver=ver)
+def just_install(pkg, ver='', prints=False):
+  s(f'python -m pip{ver} install {pkg}', prints=prints)
 
 
-def pkg_install(pkg, ver=''):
-  updatepip(ver=ver)
-  just_install(pkg, ver=ver)
-  updatepip(ver=ver)
-  s(f'python -m pip{ver} uninstall {pkg}')
-  updatepip(ver=ver)
-  just_install(pkg, ver=ver)
-  updatepip(ver=ver)
-  just_install(f'--upgrade {pkg}', ver=ver)
+def updatepip(ver='', prints=False):
+  just_install(f'--upgrade pip{ver}', ver=ver, prints=prints)
 
 
-def offline_install(pkg, ver=''):
-  just_install(f'--no-index -f . {pkg}', ver=ver)
+def pkg_install(pkg, ver='', prints=False):
+  updatepip(ver=ver, prints=prints)
+  just_install(pkg, ver=ver, prints=prints)
+  updatepip(ver=ver, prints=prints)
+  s(f'python -m pip{ver} uninstall {pkg}', prints=prints)
+  updatepip(ver=ver, prints=prints)
+  just_install(pkg, ver=ver, prints=prints)
+  updatepip(ver=ver, prints=prints)
+  just_install(f'--upgrade {pkg}', ver=ver, prints=prints)
 
 
-def pkgs(func=pkg_install, ver=''):
-  func('numpy', ver=ver)
-  func('tesseract', ver=ver)
-  func('pillow', ver=ver)
-  func('opencv-python', ver=ver)
+def offline_install(pkg, ver='', prints=False):
+  just_install(f'--no-index -f . {pkg}', ver=ver, prints=prints)
 
 
-def download_pkg(pkg, ver=''):
-  s(f'python -m pip download {pkg}')
+def pkgs(func=pkg_install, ver='', prints=False):
+  func('numpy', ver=ver, prints=prints)
+  func('tesseract', ver=ver, prints=prints)
+  func('pillow', ver=ver, prints=prints)
+  func('opencv-python', ver=ver, prints=prints)
 
 
-def main_with_ver(ver):
+def download_pkg(pkg, ver='', prints=False):
+  s(f'python -m pip download {pkg}', prints=prints)
+
+
+def main_with_option(ver, debug):
   if len(a) > 1:
     cmds = a[1]
     if cmds == 'downloads':
       mkdir('pkgs')
       cd('pkgs')
-      pkgs(func=download_pkg, ver=ver)
+      pkgs(func=download_pkg, ver=ver, prints=debug)
     elif cmds == 'offline':
       cd('pkgs')
-      pkgs(func=offline_install, ver=ver)
+      pkgs(func=offline_install, ver=ver, prints=debug)
     else:
       print(f'wrong parameter {cmds}, it must be downloads or offline or not')
   else:
-    pkgs(ver=ver)
+    pkgs(ver=ver, prints=debug)
 
 
 def main():
-  main_with_ver('3')
+  main_with_option('3', True)
 
 
 if __name__ == "__main__": main()
