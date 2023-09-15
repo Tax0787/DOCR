242c926b5c3d3e518b11969a5553f83e08499950
Tax0787 <93974004+Tax0787@users.noreply.github.com>
2023-09-15 07:39:27 +0000

    bug fix2

--- pkg_installer.py
+++ pkg_installer.py
@@ -4,51 +4,58 @@ from os import chdir as cd
 from sys import argv as a
 
 
-def just_install(pkg):
-  s(f'python -m pip install {pkg}')
+def just_install(pkg, ver=''):
+  s(f'python -m pip{ver} install {pkg}')
 
 
-def pkg_install(pkg):
-  just_install('--upgrade pip')
-  s(pkg)
-  just_install('--upgrade pip')
-  s(f'python -m pip uninstall {pkg}')
-  just_install('--upgrade pip')
-  s(pkg)
-  just_install('--upgrade pip')
-  s(f'--upgrade {pkg}')
+def updatepip(ver=''):
+  just_install(f'--upgrade pip{ver}', ver=ver)
 
 
-def offline_install(pkg):
-  just_install(f'--no-index -f . {pkg}')
+def pkg_install(pkg, ver=''):
+  updatepip(ver=ver)
+  just_install(pkg, ver=ver)
+  updatepip(ver=ver)
+  s(f'python -m pip{ver} uninstall {pkg}')
+  updatepip(ver=ver)
+  just_install(pkg, ver=ver)
+  updatepip(ver=ver)
+  just_install(f'--upgrade {pkg}', ver=ver)
 
 
-def pkgs(func=pkg_install):
-  func('numpy')
-  func('tesseract')
-  func('pillow')
-  func('opencv-python')
+def offline_install(pkg, ver=''):
+  just_install(f'--no-index -f . {pkg}', ver=ver)
 
 
-def download_pkg(pkg):
+def pkgs(func=pkg_install, ver=''):
+  func('numpy', ver=ver)
+  func('tesseract', ver=ver)
+  func('pillow', ver=ver)
+  func('opencv-python', ver=ver)
+
+
+def download_pkg(pkg, ver=''):
   s(f'python -m pip download {pkg}')
 
 
-def main():
-  print(a)
+def main_with_ver(ver):
   if len(a) > 1:
     cmds = a[1]
     if cmds == 'downloads':
       mkdir('pkgs')
       cd('pkgs')
-      pkgs(func=download_pkg)
+      pkgs(func=download_pkg, ver=ver)
     elif cmds == 'offline':
       cd('pkgs')
-      pkgs(func=offline_install)
+      pkgs(func=offline_install, ver=ver)
     else:
       print(f'wrong parameter {cmds}, it must be downloads or offline or not')
   else:
-    pkgs()
+    pkgs(ver=ver)
+
+
+def main():
+  main_with_ver('3')
 
 
 if __name__ == "__main__": main()
