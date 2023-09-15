8a0c467560ac8d39bf8ab79266bd5c6425a28f9b
Tax0787 <93974004+Tax0787@users.noreply.github.com>
2023-09-15 07:27:49 +0000

    make_pkg_installer

---
+++ pkg_installer.py
@@ -0,0 +1,50 @@
+from os import system as s
+from os import mkdir
+from os import chdir as cd
+
+
+def just_install(pkg):
+  s(f'python -m pip install {pkg}')
+
+
+def pkg_install(pkg):
+  just_install('--upgrade pip')
+  s(pkg)
+  just_install('--upgrade pip')
+  s(f'python -m pip uninstall {pkg}')
+  just_install('--upgrade pip')
+  s(pkg)
+  just_install('--upgrade pip')
+  s(f'--upgrade {pkg}')
+
+
+def offline_install(pkg):
+  just_install(f'--no-index -f . {pkg}')
+
+
+def pkgs(func=pkg_install):
+  func('numpy')
+  func('tesseract')
+  func('pillow')
+  func('opencv-python')
+
+
+def download_pkg(pkg):
+  s(f'python -m pip download {pkg}')
+
+
+def main():
+  from sys import argv as a
+  if len(a) > 1:
+    cmds = a[1]
+    if cmds == 'downloads':
+      mkdir('pkgs')
+      cd('pkgs')
+      pkgs(func=download_pkg)
+    elif cmds == 'offline':
+      cd('pkgs')
+      pkgs(func=offline_install)
+    else:
+      print(f'wrong parameter {cmds}, it must be downloads or offline or not')
+  else:
+    pkgs()
